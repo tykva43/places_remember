f051fb25713e0be4d3729ebc428b7f7ef20b4c9d
tykva43 <krikunova.olga@mail.ru>
2021-05-13 07:28:06 +0700

    Add home page view

--- places_remember/views.py
+++ places_remember/views.py
@@ -1,11 +1,10 @@
 from django.contrib.auth.decorators import login_required
-from django.http import HttpResponse
 from django.shortcuts import render
 
 
 @login_required
 def home(request):
-    return HttpResponse('aaa')
+    return render(request, 'places_remember/home.html')
 
 
 def privacy(request):
