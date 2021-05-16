3c8230bba6f586e00cc9ca48bfb87cc5e35956ad
tykva43 <krikunova.olga@mail.ru>
2021-05-17 02:27:50 +0700

    Change github workflow settings
    Added environment data to workflow config.
    Edited example of environment variables file (example.env).

--- project/settings.py
+++ project/settings.py
@@ -32,10 +32,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env.str('DJANGO_SECRET_KEY')
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env.bool('DJANGO_DEBUG')
+DEBUG = os.environ.get('DJANGO_DEBUG')
 
 ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS').split()
 
